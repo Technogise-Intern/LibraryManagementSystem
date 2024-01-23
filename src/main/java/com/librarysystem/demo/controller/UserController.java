23a4dd68fd55f9c6a8507bf9c568569c54b6dab6
adi <adityavshedge3101@gmail.com>
2024-01-23 16:04:49 +0530

    Library Controller: Moved create and login user to user controller

--- src/main/java/com/librarysystem/demo/controller/UserController.java
+++ src/main/java/com/librarysystem/demo/controller/UserController.java
@@ -3,6 +3,8 @@ package com.librarysystem.demo.controller;
 import com.librarysystem.demo.service.UserService;
 import com.librarysystem.demo.model.User;
 
+import java.sql.SQLException;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,10 +13,26 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private UserService userService;
+    private User user = new User();
+
+    public User createUser(String usernname, String password) {
+        user.setUsername(usernname);
+        user.setPassword(password);
+
+        UserService.createUser(user);
+        return user;
+    }
+
+    public String loginUser(String usernname, String password) throws SQLException {
+        user.setUsername(usernname);
+        user.setPassword(password);
+
+        String userToken = UserService.loginUser(user);
+        return userToken;
+    }
 
     @GetMapping("/user/{id}")
     public User getUser(@PathVariable Integer uid) {
-//        return userService.getUser();
         return null;
     }
 
