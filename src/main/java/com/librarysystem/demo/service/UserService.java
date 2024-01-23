bb073eba132e8c85debd2a78a15d9b309dbf8661
adi <adityavshedge3101@gmail.com>
2024-01-23 17:22:16 +0530

    User Service: Fixed Formatting

--- src/main/java/com/librarysystem/demo/service/UserService.java
+++ src/main/java/com/librarysystem/demo/service/UserService.java
@@ -20,50 +20,7 @@ public class UserService {
 
     private static User user;
     private final static String secretKey = "yourSecretKey";
-    private long expirationTimeMillis = System.currentTimeMillis() + 3600000;
-
-    public static void createUser(User user) {
-        try {
-            UserRepository.create(user);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static String loginUser(String username, String bookname) throws SQLException {
-        user = UserRepository.loginUser(username, bookname);
-        return generateJWTToken();
-    }
-
-    public static String generateJWTToken() {
-        String token = String.valueOf(
-                        Jwts.builder()
-                                .setHeaderParam("typ","JWT")
-                                .signWith(SignatureAlgorithm.RS256, secretKey)
-                                .setExpiration(new Date(expirationTimeMillis))
-                                .setSubject(user.getUsername()).compact());
-        return token;
-    }
-
-    public static List<Book> viewBooks() throws SQLException {
-        List<Book> books = new ArrayList<>();
-        return books;
-    }
-
-    public boolean canBorrow(User user) {
-        // return user.borrowedBooks.size() <= 2;
-        return false;
-    }
-
-    public static Book borrowBook(String username, String bookname) {
-
-        return null;
-    }
-
-    public List<Book> getUserBooks(User user) {
-        // return UserRepository.getUserBooks(user);
-        return null;
-    }
+    private static long expirationTimeMillis = System.currentTimeMillis() + 3600000;
 
     private List<User> users;
 
@@ -94,5 +51,48 @@ public class UserService {
 
             }
         }
+    }
+
+    public static void createUser(User user) {
+        try {
+            UserRepository.create(user);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String loginUser(String username, String bookname) throws SQLException {
+        user = UserRepository.loginUser(username, bookname);
+        return generateJWTToken();
+    }
+
+    public static String generateJWTToken() {
+        String token = String.valueOf(
+                Jwts.builder()
+                        .setHeaderParam("typ", "JWT")
+                        .signWith(SignatureAlgorithm.RS256, secretKey)
+                        .setExpiration(new Date(expirationTimeMillis))
+                        .setSubject(user.getUsername()).compact());
+        return token;
+    }
+
+    public static List<Book> viewBooks() throws SQLException {
+        List<Book> books = new ArrayList<>();
+        return books;
+    }
+
+    public boolean canBorrow(User user) {
+        // return user.borrowedBooks.size() <= 2;
+        return false;
+    }
+
+    public static Book borrowBook(String username, String bookname) {
+
+        return null;
+    }
+
+    public List<Book> getUserBooks(User user) {
+        // return UserRepository.getUserBooks(user);
+        return null;
     }
 }
